7606dda18c2c16364f32a887a52726f0b59e987b
Robert Walz <abitdevelopment@gmail.com>
2021-10-26 16:48:26 +0200

    init

--- src/main/java/org/rbrtwlz/edgedetection/ImageWriter.java
+++ src/main/java/org/rbrtwlz/edgedetection/ImageWriter.java
@@ -7,11 +7,8 @@ import javax.imageio.ImageIO;
 public class ImageWriter {
 
   public static void writeImage(String imagePath, ImageArray imgArray, String formatString){
-
     BufferedImage bi = new BufferedImage(imgArray.getWidth(), imgArray.getHeight(), BufferedImage.TYPE_INT_RGB);
-
     int[][][] pa = imgArray.getPixelArray();
-
     for(int i=0; i<imgArray.getWidth(); i++) {
       for(int j=0; j< imgArray.getHeight(); j++) {
           int rgbPixel = (int)pa[0][j][i]<<16 | (int)pa[1][j][i] << 8 | (int)pa[2][j][i]; // TODO ? 
