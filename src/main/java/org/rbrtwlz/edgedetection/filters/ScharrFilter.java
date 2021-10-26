a4ff97b293d66d0266ef4f295cdf576adf29ec2f
Robert Walz <abitdevelopment@gmail.com>
2021-10-26 18:58:54 +0200

    init

--- src/main/java/org/rbrtwlz/edgedetection/filters/ScharrFilter.java
+++ src/main/java/org/rbrtwlz/edgedetection/filters/ScharrFilter.java
@@ -6,7 +6,6 @@ public class ScharrFilter extends SlidingWindowFilter{
 
   private final int[][] k = {{47,162,47}, {0,0,0}, {-47,-162,-47}};
   private Kernel kernel = new Kernel(k);
-  private SobelFilter sobelFilter;
 
   @Override
   public ImageArray applyFilter(ImageArray imageArray) {
